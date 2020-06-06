b0e2b5570805eaa3d968b33f783df192fbbe0738
intxparts <jcowens.dx@gmail.com>
2020-06-06 16:41:36 -0400

    target .net 4.7.2. make properties pane expand/collapse.

--- Slab/MainWindowViewModel.cs
+++ Slab/MainWindowViewModel.cs
@@ -12,67 +12,6 @@ namespace Slab
 {
     public sealed class MainWindowViewModel : INotifyPropertyChanged
     {
-        private GLDataContext _glDataContext;
-        private DataModel _dataModel;
-        private LicenseData _licenseData;
-
-        public DataModel DataModel { get { return _dataModel; } }
-        public LicenseData LicenseData { get { return _licenseData; } }
-        public GLDataContext GLDataContext { get { return _glDataContext; } }
-        public List<ITool> Tools { get; private set; }
-
-        private ITool _selectedTool;
-        public ITool SelectedTool
-        {
-            get
-            {
-                return _selectedTool;
-            }
-            set
-            {
-                if (_selectedTool != value)
-                {
-                    _selectedTool = value;
-                    OnSelectedToolChanged();
-                    NotifyPropertyChanged(nameof(SelectedTool));
-                }
-            }
-        }
-
-        public DelegateCommand Window_OnLoaded { get; private set; }
-
-        public void OnWindowLoaded()
-        {
-            SelectedTool = Tools.First();
-        }
-
-        public void OnSelectedToolChanged()
-        {
-            // instantiate its view and view model using the activator
-            var viewModel = Activator.CreateInstance(SelectedTool.DefinedViewModelType);
-            var view = Activator.CreateInstance(SelectedTool.DefinedViewType);
-            var usrCtrl = view as UserControl;
-            var vm = viewModel as IViewModel;
-            if (vm == null || usrCtrl == null)
-                return;
-
-            vm.Initialize(_glDataContext, _dataModel, _licenseData);
-
-            // bind the view data context to the view model
-            usrCtrl.DataContext = vm;
-
-            // raise request load tool and pass in the view as an argument to the event
-            RequestLoadTool?.Invoke(this, usrCtrl);
-        }
-
-        public event EventHandler<UserControl> RequestLoadTool;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         public MainWindowViewModel()
         {
             Window_OnLoaded = new DelegateCommand(OnWindowLoaded);
@@ -103,6 +42,116 @@ namespace Slab
             foreach (var tool in Tools)
                 tool.Initialize(_dataModel, _licenseData);
 
+            _isPropertiesPaneExpanded = true;
+            _selectedTool = null;
+            _onPropertiesPaneExpandCommand = new DelegateCommand(OnPropertiesPaneExpand);
+            _onPropertiesPaneCollapseCommand = new DelegateCommand(OnPropertiesPaneCollapse);
+        }
+
+        private void OnPropertiesPaneCollapse()
+        {
+            IsPropertiesPaneExpanded = false;
+        }
+
+        private void OnPropertiesPaneExpand()
+        {
+            IsPropertiesPaneExpanded = true;
+        }
+
+        private GLDataContext _glDataContext;
+        private DataModel _dataModel;
+        private LicenseData _licenseData;
+
+        private readonly DelegateCommand _onPropertiesPaneCollapseCommand;
+        private readonly DelegateCommand _onPropertiesPaneExpandCommand;
+
+        public DelegateCommand OnPropertiesPaneCollapseCommand => _onPropertiesPaneCollapseCommand;
+        public DelegateCommand OnPropertiesPaneExpandCommand => _onPropertiesPaneExpandCommand;
+
+        public DataModel DataModel { get { return _dataModel; } }
+        public LicenseData LicenseData { get { return _licenseData; } }
+        public GLDataContext GLDataContext { get { return _glDataContext; } }
+
+        private bool _isPropertiesPaneExpanded;
+        public bool IsPropertiesPaneExpanded
+        {
+            get
+            {
+                return _isPropertiesPaneExpanded;
+            }
+            set
+            {
+                _isPropertiesPaneExpanded = value;
+                NotifyPropertyChanged(nameof(IsPropertiesPaneExpanded));
+                NotifyPropertyChanged(nameof(IsPropertiesPaneCollapsed));
+            }
+        }
+        public bool IsPropertiesPaneCollapsed
+        {
+            get
+            {
+                return !_isPropertiesPaneExpanded;
+            }
+        }
+
+
+        public List<ITool> Tools { get; private set; }
+
+        private ITool _selectedTool;
+        public ITool SelectedTool
+        {
+            get
+            {
+                return _selectedTool;
+            }
+            set
+            {
+                // on changing tools
+                if (_selectedTool != value)
+                {
+                    _selectedTool = value;
+                }
+                // on selecting the same tool
+                else
+                {
+                    _selectedTool = null;
+                }
+                OnSelectedToolChanged();
+                NotifyPropertyChanged(nameof(SelectedTool));
+            }
+        }
+
+        public DelegateCommand Window_OnLoaded { get; private set; }
+
+        public void OnWindowLoaded()
+        {
+        }
+
+        public void OnSelectedToolChanged()
+        {
+            // instantiate its view and view model using the activator
+            var viewModel = Activator.CreateInstance(SelectedTool.DefinedViewModelType);
+            var view = Activator.CreateInstance(SelectedTool.DefinedViewType);
+            var usrCtrl = view as UserControl;
+            var vm = viewModel as IViewModel;
+            if (vm == null || usrCtrl == null)
+                return;
+
+            vm.Initialize(_glDataContext, _dataModel, _licenseData);
+
+            // bind the view data context to the view model
+            usrCtrl.DataContext = vm;
+
+            // raise request load tool and pass in the view as an argument to the event
+            RequestLoadTool?.Invoke(this, usrCtrl);
+        }
+
+        public event EventHandler<UserControl> RequestLoadTool;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
